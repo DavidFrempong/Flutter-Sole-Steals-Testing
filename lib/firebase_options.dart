1de34e1307412b857db05cfbaae5ff6a4d9b56a8
CodigcarMatrix <carlos.castilla.matrix@gmail.com>
2023-04-25 01:40:45 -0400

    cambio a ios.app.solesteal1

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -54,11 +54,11 @@ class DefaultFirebaseOptions {
 
   static const FirebaseOptions ios = FirebaseOptions(
     apiKey: 'AIzaSyA_Xpnh5H8IdAWmjSJGK3bTASH8G_5bOt8',
-    appId: '1:395841594471:ios:d98ba4fc011ee04e9620c2',
+    appId: '1:395841594471:ios:073d9b7fad95c3589620c2',
     messagingSenderId: '395841594471',
     projectId: 'push-notification-b1ddc',
     storageBucket: 'push-notification-b1ddc.appspot.com',
-    iosClientId: '395841594471-59oevqqbef4idav79d93ur702uqh8ouj.apps.googleusercontent.com',
-    iosBundleId: 'com.whiz.apn',
+    iosClientId: '395841594471-eov3871f76cng77chen8n9bq8dabtbus.apps.googleusercontent.com',
+    iosBundleId: 'ios.app.solesteal1',
   );
 }
